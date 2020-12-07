8d9adb8f28a8d0d979dcc6be0ef8267026c3e372
GraceOyiza <graceoyiza13@gmail.com>
2020-12-07 14:34:57 +0100

    Create dislike model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_091415) do
+ActiveRecord::Schema.define(version: 2020_12_07_132912) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2020_12_07_091415) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "dislikes", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
   end
 
   create_table "friendships", force: :cascade do |t|

3fb37259f9fcefe2038bc4b5dee6d03b89617191
GraceOyiza <graceoyiza13@gmail.com>
2020-12-07 14:28:41 +0100

    Create model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,51 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  # has_many :friendships, -> { where('confirmed = ?', true) },
+  # class_name: 'Friendship', dependent: :destroy
+  # has_many :friends, through: :friendships
+
+  # has_many :pending_friendships, -> { where('confirmed = ?', false) },
+  #   class_name: 'Friendship', foreign_key: 'user_id', dependent: :destroy
+
+  # has_many :pending_friends, through: :pending_friendships, source: :friend
+
+  # has_many :inverse_friendships, -> { where('confirmed = ?', false) },
+  #   class_name: 'Friendship', foreign_key: 'friend_id', dependent: :destroy
+
+  # has_many :friend_requests, through: :inverse_friendships, source: :user
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  
+  # Users who have yet to confirme friend requests
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+end
+
+def confirm_friend(user)
+  friendship = inverse_friendships.find{|friendship| friendship.user == user}
+  friendship.confirmed = true
+  friendship.save
+end
+
+def friend?(user)
+  friends.include?(user)
+end
+
+
 end
